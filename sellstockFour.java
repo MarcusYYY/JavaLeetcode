ee942a4118c7db19cb2afdb63cfb56e616d7172d
Marcus Yu <marcusyuzc1992@gmail.com>
2016-03-29 09:58:50 -0500

    Greedy + DP

---
+++ sellstockFour.java
@@ -0,0 +1,22 @@
+public class sellstockFour {
+    public int maxProfit(int k, int [] prices){
+        if(k >= prices.length/2){
+        int maxPrfit = 0;
+        for(int i = 1 ; i < prices.length; i++){
+          if(prices[i] > prices[i-1]) maxProfit += prices[i] - prices[i-1];
+          }
+          return maxProfit;
+      }
+
+      int[] maxProfit = new int[k+1];
+      int[] lowPrice = new int[k+1];
+      for(int i = 0 ; i < lowPrice.length ; i++) lowPrice[i] = Integer.MAX_VALUE;
+      for(int p : prices){
+          for(int i = k; i >= 1 ; i--){
+              maxProfit[i] = Math.max(maxProfit[i], p - lowPrice[i]);
+              lowPrice[i] = Math.min(lowPrice[i], p - maxProfit[i-1]);
+          }
+      }
+      return maxProfit[k];
+  }
+}
